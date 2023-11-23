041dbf89c3e974f20edd1c8ff0f15e851d6988b7
unknown <b00138240@mytudublin.ie>
2023-11-23 11:29:49 +0000

    part1

---
+++ processForm1.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = $_POST['firstname'];
+    $surname = $_POST['surname'];
+    $luggage = isset($_POST['luggage']) ? 1 : 0;
+
+    $_SESSION['passengerFN'] = $firstname;
+    $_SESSION['passengerSN'] = $surname;
+    $_SESSION['luggage'] = $luggage;
+
+    if ($luggage == 1) {
+        header("Location: luggage.html");
+        exit();
+    } else {
+        header("Location: finalStep.php");
+        exit();
+    }
+} else {
+    echo "Invalid request method";
+}
+?>
