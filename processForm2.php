9b553f9f335bc959580745f31969b48f4ab78722
unknown <b00138240@mytudublin.ie>
2023-11-23 13:28:27 +0000

    part3

---
+++ processForm2.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $subTenKG = $_POST['subTenKG'];
+    $overTenKG = $_POST['overTenKG'];
+
+    $_SESSION['subTenKG'] = $subTenKG;
+    $_SESSION['overTenKG'] = $overTenKG;
+
+    header("Location: finalStep.php");
+    exit();
+} else {
+    echo "Invalid request method";
+}
+?>
